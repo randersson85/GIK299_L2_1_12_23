aae2484e44cf063046863933fe5d30fc4b4dbc81
Robert Andersson <h23roban@du.se>
2023-12-01 09:31:44 +0100

    first commit

---
+++ GIK299_L2_GIT/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GIK299_L2_GIT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
